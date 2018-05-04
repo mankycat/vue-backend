43cd96696b46ff50955060d8086909cb38922204
mankycat <bruce0115@gmail.com>
2018-05-04 16:50:18 +0800

    local initial commit

---
+++ src/main/java/brucewu/cc/church/ChurchApplication.java
@@ -0,0 +1,12 @@
+package brucewu.cc.church;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchApplication.class, args);
+	}
+}
---
+++ src/test/java/brucewu/cc/church/ChurchApplicationTests.java
@@ -0,0 +1,16 @@
+package brucewu.cc.church;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChurchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
